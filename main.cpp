fd56971f5f9063899807c8c3c0890c56a8f2c783
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-18 14:14:02 +0000

    [Yudi] - simple vector

--- main.cpp
+++ main.cpp
@@ -1,16 +1,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int add (int a, int b){
-  int total;
-  return total = a + b;
-}
-
 int main(){
-  int x, y, c;
-  x = 10;
-  y = 20;
-  c = add(x, y);
-  cout <<c<< endl;
+  vector<int> v {2, 3,5};
+  for(int i = 0; i < v.size(); i++){
+    cout << v[i] << " ";
+  }
   return 0;
 }
