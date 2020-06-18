0bd038f6eeed53f002da16fe1c140403f93ff8e0
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-18 22:46:48 +0000

    [Yudi] - stl lower bound

--- main.cpp
+++ main.cpp
@@ -1,10 +1,27 @@
-#include <bits/stdc++.h>
+#include <bits/stdc++.h>       
 using namespace std;
 
-int main(){
-  vector<int> v {2, 3,5};
-  for(int i = 0; i < v.size(); i++){
-    cout << v[i] << " ";
-  }
-  return 0;
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */ 
+    int len, data; 
+    cin >> len;
+    vector<int> v;
+    for(int i = 0; i < len; i++){
+        cin >> data;
+        v.push_back(data);
+    }
+    int q, val;
+    cin >> q;
+
+    for(int i = 0; i < q; i++){
+      cin >> val;
+      vector<int>::iterator low;
+      low = lower_bound(v.begin(), v.end(), val);
+      if (v[low - v.begin()] == val)
+           cout << "Yes " << (low - v.begin()+1) << endl;
+       else
+           cout << "No " << (low - v.begin()+1) << endl;
+    }
+    return 0;
 }
