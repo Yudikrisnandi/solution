af89280b739c905a1ea5b9b8ab367121e4c1267c
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-20 12:12:28 +0000

    [Yudi] - simple pointer

--- main.cpp
+++ main.cpp
@@ -3,24 +3,10 @@ using namespace std;
 
 
 int main() {
-    int len, data; 
-    cin >> len;
-    vector<int> v;
-    for(int i = 0; i < len; i++){
-        cin >> data;
-        v.push_back(data);
-    }
-    int q, val;
-    cin >> q;
-
-    for(int i = 0; i < q; i++){
-      cin >> val;
-      vector<int>::iterator low;
-      low = lower_bound(v.begin(), v.end(), val);
-      if (v[low - v.begin()] == val)
-           cout << "Yes " << (low - v.begin()+1) << endl;
-       else
-           cout << "No " << (low - v.begin()+1) << endl;
-    }
+    int x = 10;
+    int *ptr = &x;
+    ++*ptr;
+    cout << *ptr << endl;
+    cout << x << endl;
     return 0;
 }
