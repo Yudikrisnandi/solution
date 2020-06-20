826e651db82c99fe0f7edc3ac7304b0e27e02474
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-20 13:44:42 +0000

    [Yudi] - operator overloading

--- main.cpp
+++ main.cpp
@@ -1,15 +1,24 @@
 #include <bits/stdc++.h>       
 using namespace std;
-#define say(x) #x
-#define love true
-#ifndef YK
-  #define YK 18
-#endif
+
+class Complex {
+  public:
+    int real;
+    int img;
+  Complex add(Complex c){
+    Complex temp;
+    temp.real = real + c.real;
+    temp.img = img + c.img;
+    return temp;
+  }
+};
 
 
 int main() {
-    cout << say(yk) << endl;
-    cout << love << endl;
-    cout << YK << endl;
+    Complex c1, c2, c3;
+    c1.real = 5; c1.img=4;
+    c2.real = 3; c2.img=2;
+    c3 = c1.add(c2);
+    cout << c3.real << " " << c3.img << endl;
     return 0;
 }
