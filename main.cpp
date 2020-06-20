a65b13dfc731ebd4bd13c766eb997c4a6231bb28
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-20 13:46:47 +0000

    [Yudi] - operator overloading second

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ class Complex {
   public:
     int real;
     int img;
-  Complex add(Complex c){
+  Complex operator+(Complex c){
     Complex temp;
     temp.real = real + c.real;
     temp.img = img + c.img;
@@ -18,7 +18,7 @@ int main() {
     Complex c1, c2, c3;
     c1.real = 5; c1.img=4;
     c2.real = 3; c2.img=2;
-    c3 = c1.add(c2);
+    c3 = c1 + c2;
     cout << c3.real << " " << c3.img << endl;
     return 0;
 }
