50034801e09fde44d5e09880c217d220be090f39
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-24 04:47:14 +0000

    [Yudi] - struct

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include <bits/stdc++.h>       
 using namespace std;
 
-class Complex {
-  public:
-    int real;
-    int img;
-  Complex operator+(Complex c){
-    Complex temp;
-    temp.real = real + c.real;
-    temp.img = img + c.img;
-    return temp;
+struct Number {
+  int x;
+  int y;
+  int display(){
+    return x + y;
   }
 };
 
-
 int main() {
-    Complex c1, c2, c3;
-    c1.real = 5; c1.img=4;
-    c2.real = 3; c2.img=2;
-    c3 = c1 + c2;
-    cout << c3.real << " " << c3.img << endl;
-    return 0;
+  Number num;
+  num.x = 8;
+  num.y= 7;
+  cout << num.display() << endl;
+  
+  return 0;
 }
