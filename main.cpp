7820cf6e69d200016e140d0e922a6644820f4324
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-24 08:43:36 +0000

    [Yudi] - simple pointer

--- main.cpp
+++ main.cpp
@@ -1,25 +1,13 @@
 #include <bits/stdc++.h>       
 using namespace std;
 
-class Rectangle {
-  public:
-    int length;
-    int breadth;
-
-    int area() {
-      return length * breadth;
-    }
-
-    int perimeter() {
-      return 2 * (length + breadth);
-    }
-};
 
 int main() {
-  Rectangle *ptr = new Rectangle;
-  ptr -> length = 3;
-  ptr -> breadth = 4;
-
-  cout << "area : " << ptr -> area() << endl;
-  cout << "perimeter : " << ptr -> perimeter() << endl;
+  int a = 5;
+  int *ptra;
+  ptra = &a;
+  cout << &a << endl;
+  cout << &ptra << endl;
+  cout << *ptra << endl;
+  return 0;
 }
