dc186761a1178bb019539234e101accec4abd01d
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-28 08:26:35 +0000

    [Yudi] - update complex class

--- main.cpp
+++ main.cpp
@@ -2,10 +2,26 @@
 using namespace std;
 
 class Rectangle {
-  public:
+  private:
     int length;
     int breadth;
-
+  public:
+    Rectangle(int l = 0,int b = 0){
+      length = l;
+      breadth = b;
+    }
+    int getLength(){
+      return length;
+    }
+    int getBreadth(){
+      return breadth;
+    }
+    void setLength(int l){
+      length = l;
+    }
+    void setBreadth(int b){
+      breadth = b;
+    }
     int area() {
       return length * breadth;
     }
@@ -17,8 +33,8 @@ class Rectangle {
 
 int main() {
   Rectangle *ptr = new Rectangle;
-  ptr -> length = 3;
-  ptr -> breadth = 4;
+  ptr -> setLength(3);
+  ptr -> setBreadth(4);
 
   cout << "area : " << ptr -> area() << endl;
   cout << "perimeter : " << ptr -> perimeter() << endl;
