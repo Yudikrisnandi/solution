b0728f06c8bd584c6a60f0013038cbe43c87ba63
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-06-28 08:21:27 +0000

    [Yudi] - char

--- main.cpp
+++ main.cpp
@@ -3,16 +3,7 @@ using namespace std;
 
 
 int main() {
-  int *p;
-  p = new int[3];
-  p[0] = 1;
-  p[1] = 2;
-  p[2] = 3;
-  for(int i = 0; i < 3; i++) {
-    cout << p[i] << " " ;
-  }
-  cout << endl;
-  delete []p;
-  p = nullptr;
+  char space = ' ';
+  cout << string(7, space);
   return 0;
 }
