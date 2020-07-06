f30d274f1bf314ae88498f7822c73eff0db71ad0
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-07-06 01:37:18 +0000

    simple linked list

--- main.cpp
+++ main.cpp
@@ -1,9 +1,18 @@
 #include <bits/stdc++.h>       
 using namespace std;
 
+struct Node {
+  int data;
+  Node *next;
+};
+
+Node *head = NULL;
+
 
 int main() {
-  char space = ' ';
-  cout << string(7, space);
+  Node *ptr = new Node();
+  ptr -> data = 1;
+  ptr -> next = NULL;
+  head = ptr;
   return 0;
 }
