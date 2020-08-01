1b7a056fdd6ebb540bc7c5114247e7a8261b2d73
Yudikrisnandi <yudikrisnandi22@gmail.com>
2020-08-01 10:11:01 +0000

    add solution

--- main.cpp
+++ main.cpp
@@ -1,41 +1,24 @@
-#include <bits/stdc++.h>       
+/**
+ *    author:  ykaka
+ *    created: 01.08.2020 13:34:48       
+**/
+ 
+#include <bits/stdc++.h>
 using namespace std;
-
-class Rectangle {
-  private:
-    int length;
-    int breadth;
-  public:
-    Rectangle(int l = 0,int b = 0){
-      length = l;
-      breadth = b;
+ 
+int main(){
+  int a = 1;
+  int b = 7;
+  int i = 0;
+  bool year = true;
+  while(year){
+    a *= 3;
+    b *= 2;
+    i++;
+    if(a > b){
+      year = false;
     }
-    int getLength(){
-      return length;
-    }
-    int getBreadth(){
-      return breadth;
-    }
-    void setLength(int l){
-      length = l;
-    }
-    void setBreadth(int b){
-      breadth = b;
-    }
-    int area() {
-      return length * breadth;
-    }
-
-    int perimeter() {
-      return 2 * (length + breadth);
-    }
-};
-
-int main() {
-  Rectangle *ptr = new Rectangle;
-  ptr -> setLength(3);
-  ptr -> setBreadth(4);
-
-  cout << "area : " << ptr -> area() << endl;
-  cout << "perimeter : " << ptr -> perimeter() << endl;
+  }
+  cout << i << endl;
+  return 0;
 }
