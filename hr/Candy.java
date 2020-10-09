86efe2a75d7db618a39e7d20bb201679761a9dcf
Yudi Krisnandi <yudikrisnandi22@gmail.com>
2020-10-09 08:40:56 +0800

    add new solution

---
+++ hr/Candy.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Candy {
+  public static void main(String[] args){
+    System.out.println("Helo");
+    Scanner scanner = new Scnanner(System.in);
+  }
+}
+
