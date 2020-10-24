8cb28150adf0183d3ef45aa6bb0af22b306a3ad1
Yudi krisnandi <yudikrisnandi22@gmail.com>
2020-10-24 10:56:22 +0800

    add dir

--- hr/Candy.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Candy {
-  public static void main(String[] args){
-    System.out.println("Helo");
-    Scanner scanner = new Scnanner(System.in);
-  }
-}
-
